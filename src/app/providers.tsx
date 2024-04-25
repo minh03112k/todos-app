00e126a5b6f4f0dcd8b4e724d904e765cb08bbc3
minh03112k <hoangminh03112k@gmail.com>
2024-04-25 22:59:24 +0700

    upgrade project, add tan stack dev tool

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,13 +3,15 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
         // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
+        // above 0 to avoid re-fetching immediately on the client
         staleTime: 60 * 1000,
       },
     },
@@ -24,7 +26,7 @@ function getQueryClient() {
     return makeQueryClient();
   } else {
     // Browser: make a new query client if we don't already have one
-    // This is very important so we don't re-make a new client if React
+    // This is very important, so we don't re-make a new client if React
     // suspends during the initial render. This may not be needed if we
     // have a suspense boundary BELOW the creation of the query client
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
@@ -44,6 +46,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <NextUIProvider>
         {children}
       </NextUIProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
